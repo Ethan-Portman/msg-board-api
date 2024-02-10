3876fff02f8cc37bf05a147260072d2c4eb6e8a2
EthanPortman <ethanportman@gmail.com>
2024-02-10 14:24:45 -0800

    Added code to connect to the database

--- db.js
+++ db.js
@@ -1,2 +1,18 @@
 import mongoose from "mongoose";
 
+let dbURI = 'mongodb://mongo:27017/messageBoardDB';
+mongoose.set('strictQuery', true);
+mongoose.connect(dbURI);
+
+// print message to console when connected to DB
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+// Print error messages with connection issues
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose Connection error: ' + err);
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});

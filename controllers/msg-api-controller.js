c989e87b461433b3e3e5de18b1269e373efbf77a
EthanPortman <ethanportman@gmail.com>
2024-02-09 21:58:04 -0800

    Implemented POST Request handler

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,3 +1,5 @@
+import messageSchema from "../models/message-schema.js";
+
 let messages = [
     { id: 0, name: "Bob", msgTxt: "Hello there" },
     { id: 1, name: "Joe", msgTxt: "Hey" },
@@ -15,6 +17,18 @@ const getAllMessages = (req, res) => {
 
 // POST Request Handler
 const addNewMessage = async (req, res) => {
+    try {
+        let message = await messageSchema.validate(req.body);
+        message.id = messages.length
+        messages.unshift(message);
+        console.log(messages);
+    } catch (err) {
+        res
+            .status(400)
+            .send('Bad Request. The message in the body of the \
+            Request is either missing or malformed.');
+    }
+
     res.status(200).send('Successful API POST Request');
 };
 

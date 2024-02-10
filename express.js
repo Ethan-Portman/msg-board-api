dea28eabe4c9ba909291363c09cd2679f58b629c
EthanPortman <ethanportman@gmail.com>
2024-02-09 20:59:41 -0800

    Added a Express server with documented API

---
+++ express.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+
+// Express middleware
+app.use(express.json())
+app.use(helmet())
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());
+app.use(morgan('dev'));
+app.use(cors());
+
+// Routing
+app.get('/', (req, res) => {
+    res.send('Node.js Server is live!');
+});
+
+export default app;

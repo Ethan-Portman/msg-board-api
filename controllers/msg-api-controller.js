c5236063a8e87ea2c4b1d71843cda282984e679e
EthanPortman <ethanportman@gmail.com>
2024-02-10 11:19:10 -0800

    Fixed some bugs

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,9 +1,9 @@
 import messageSchema from "../models/message-schema.js";
 
 let messages = [
-    { id: 0, name: "Bob", msgTxt: "Hello there" },
-    { id: 1, name: "Joe", msgTxt: "Hey" },
-    { id: 2, name: "Bob", msgTxt: "How are you?" }
+    { id: 0, name: "Bob", msgText: "Hello there" },
+    { id: 1, name: "Joe", msgText: "Hey" },
+    { id: 2, name: "Bob", msgText: "How are you?" }
 ]
 
 // GET Request Handler
@@ -21,15 +21,13 @@ const addNewMessage = async (req, res) => {
         let message = await messageSchema.validate(req.body);
         message.id = messages.length
         messages.unshift(message);
-        console.log(messages);
+        res.status(200).send(message);
     } catch (err) {
         res
             .status(400)
             .send('Bad Request. The message in the body of the \
             Request is either missing or malformed.');
     }
-
-    res.status(200).send('Successful API POST Request');
 };
 
 export { getAllMessages, addNewMessage }
--- express.js
+++ express.js
@@ -8,14 +8,32 @@ import apiRouter from './routes/api-router.js';
 
 const app = express();
 
+// Define the allowed frontend origins
+const allowedOrigins = ['http://localhost:3000', 'http://172.30.71.9:3000'];
+
+// CORS configuration
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+
 // Express middleware
-app.use(express.json())
-app.use(helmet())
+app.use(express.json());
+app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors(corsOptions));
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
 
 // Router (Not in use)
 // http://localhost:3004/

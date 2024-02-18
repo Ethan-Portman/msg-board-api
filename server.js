34a1ae2c96347c96d6937209eb34de0227de162e
EthanPortman <ethanportman@gmail.com>
2024-02-17 23:56:05 -0800

    Added a summary to the file

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/*
+This file initializes a Node/Express server.
+   - Listens on port 3004 (or specified by the environment variable PORT)
+   - Routes requests to the Express app defined in the express.js file.
+*/
+
 import app from './express.js';
 
 const port = process.env.PORT || 3004;
